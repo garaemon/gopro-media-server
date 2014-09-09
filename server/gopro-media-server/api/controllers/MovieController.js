09fa3d64693d7091f849c3f1d06d1b14f5253733
jskuser <jskuser@jsk-gopro.jsk.imi.i.u-tokyo.ac.jp>
2014-09-09 12:36:16 +0900

    Add movie api

---
+++ server/gopro-media-server/api/controllers/MovieController.js
@@ -0,0 +1,10 @@
+/**
+ * MovieController
+ *
+ * @description :: Server-side logic for managing movies
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+
+};
